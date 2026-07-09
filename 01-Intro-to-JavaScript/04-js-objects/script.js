7547eb8f846b365f8dd74929c44ca68a2678a025
Patrick Pluequett <patrick.pluequett@wbscodingschool.com>
2026-07-09 10:32:23 +0200

    add intro objects

---
+++ 01-Intro-to-JavaScript/04-js-objects/script.js
@@ -0,0 +1,85 @@
+// Objekt erstellen
+const firstPerson = {
+	firstName: 'Karl',
+	age: 30,
+	friends: ['Hannah', 'John'],
+	location: {
+		city: 'berlin',
+	},
+	greet: () => console.log('hey'),
+	email: 'karl@example.org',
+};
+
+// Auf Werte im Objekt zugreifen
+const myKey = 'firstName';
+
+console.log(firstPerson.firstName);
+console.log(firstPerson['firstName']);
+console.log(firstPerson[myKey]);
+
+console.log(firstPerson.friends[0]);
+console.log(firstPerson.location.city);
+
+// Werte überschreiben oder hinzufügen
+firstPerson.firstName = 'Karla';
+firstPerson.isMarried = true;
+console.log(firstPerson.firstName);
+
+// Methode des Objekts aufrufen
+firstPerson.greet();
+
+// Key-Value Paar lösen
+// delete firstPerson.location;
+console.log(firstPerson.location);
+
+// Destructuring
+const firstName = firstPerson.firstName;
+const { firstName: personName } = firstPerson;
+console.log('person:', personName);
+
+const myFriends = [
+	{ firstName: 'Hannah', age: 20 },
+	{ firstName: 'Karla', age: 50 },
+];
+const [firstFriend, secondFriend] = myFriends;
+
+const {
+	location: { city },
+} = firstPerson;
+
+// eigebaute Objekte
+const alphabet = ['a', 'b', 'c', 'd'];
+
+const index = Math.floor(Math.random() * alphabet.length);
+console.log(alphabet[index]);
+console.log(new Date().getMonth());
+console.log(new Date().getDay());
+console.log(Object.entries(firstPerson));
+
+// Objekte kopieren
+// const secondPerson = firstPerson;
+// spread operator
+const secondPerson = { ...firstPerson, age: 100, pets: ['dog', 'cat'] };
+
+secondPerson.firstName = 'John';
+console.log(secondPerson.firstName);
+console.log(firstPerson.firstName);
+
+firstPerson.location.city = 'madrid';
+
+console.log(firstPerson.location.city);
+console.log(firstPerson.location.city);
+console.log(secondPerson.pets);
+console.log(firstPerson.pets);
+
+// rest operator
+const { email, ...persona } = firstPerson;
+console.log(firstPerson.email);
+console.log(persona.email);
+function addNumbers(a, b, ...numbers) {
+	console.log(a);
+	console.log(b);
+	console.log(numbers);
+}
+
+addNumbers(1, 2, 3, 4, 5, 6);
