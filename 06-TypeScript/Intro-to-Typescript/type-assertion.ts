73ec4290908f0c28a8ef3a0399fee3ccfde7e17c
edubur <edu.bu@gmx.de>
2026-08-12 13:55:39 +0200

    add advanced-types02 lecture content

---
+++ 06-TypeScript/Intro-to-Typescript/type-assertion.ts
@@ -0,0 +1,62 @@
+export {};
+let value: unknown = "Hello"
+const greeting = value as string;
+
+console.log(greeting.toUpperCase());
+
+// 2 Syntax Möglichkeiten
+// const first = <string>value;
+const second = value as string;
+
+// keine Konvertierung
+// unknown lässt uns jeden möglichen Wert speichern
+const valueN: unknown = 42;
+const text = valueN as string; 
+// console.log(text.toUpperCase()); 
+// Anwendung crasht, da wir eine string Methode auf eine Zahl anwenden
+
+// // DOM-Elemente präzisieren
+// const input = document.querySelector("input");
+
+// if (input instanceof HTMLInputElement) {
+//     input.value = "Ada";
+// }
+
+// JSON bzw. externe Daten
+type Post = {
+    id: number;
+    title: string;
+};
+// keine echte Validierung!
+// const response = await fetch("api/posts");
+// const posts = (await response.json()) as Post[];
+
+
+// undefined = Wert nicht gesetzt oder Property fehlt
+// null = Wert ist bewusst nicht gesetzt
+
+let username: string | null = null;
+
+type User = {
+    name: string;
+    contact: {
+        email: string;
+        phoneNumber?: string;
+    };
+    address?: {
+        street: string;
+        zipCode: string;
+    };
+};
+
+const user: User = {
+    name: "Ada",
+    contact: { email: "ada@example.com"},
+};
+
+const street = user.address?.street ?? "Street not provided"
+
+console.log(user.contact.email);
+// console.log(user.address.street); // cannot read properties of undefined
+console.log(user.address?.street);
+
---
+++ 06-TypeScript/Intro-to-Typescript/type-safety.ts
@@ -0,0 +1,125 @@
+export {};
+
+// Type Narrowing
+
+function printValue (value: string | number): void {
+    if (typeof value === "string") {
+        console.log(value.toUpperCase());
+    } else {
+    console.log(value.toFixed(2))
+    }
+};
+
+// printValue(2000);
+
+function printLength(value?: string): void {
+    if (value !== undefined) {
+        console.log(value.length);
+    } else {
+        console.log("Test");
+    }
+} 
+// printLength("");
+
+// equality narrowing 
+function compare (first: string | number, second: string | boolean):void {
+    // (type und value müssen identisch sein)
+    if ( first === second ) {
+        console.log(first.toUpperCase());
+    }
+}
+
+// compare("one", "one");
+
+// in-Operator
+
+type Dog = { bark: () => void };
+type Cat = { meow: () => void };
+type Pet = Dog | Cat;
+
+// cat dog Objekte
+const dog: Dog = {
+    bark: () => console.log("Woof!"),
+};
+
+const cat: Cat = {
+    meow: () => console.log("Meow!"),
+};
+
+function speak(pet: Pet): void {
+    if ("bark" in pet) {
+        pet.bark();
+    } else {
+        pet.meow();
+    }
+}
+
+// speak(dog);
+// speak(cat);
+
+// discriminated unions
+
+type OnlineEvent = {
+    kind: "online",
+    meetingUrl: string;
+};
+
+type OnsiteEvent = {
+    kind: "onsite";
+    address: string;
+};
+
+type EventLocation = OnlineEvent | OnsiteEvent
+
+// kind ist das discriminant Feld und enthält unterschiedliche Werte
+function printLocation(location: EventLocation): void {
+    if(location.kind === "online") {
+        console.log(location.meetingUrl);
+    } else {
+        console.log(location.address);
+    }
+}
+
+// instanceof
+function logDateOrString(value: Date | string): void {
+    if (value instanceof Date) {
+        console.log(value.toLocaleDateString("de-De"));
+    } else {
+        console.log(value.trim());
+    }
+}
+
+// custom type predicates
+
+// vehicle is Car beschreit was als true-Rückgabewert ausgegeben werden soll
+// function isCar(vehicle: Vehicle) vehicle is Car {
+//     return "drive" in vehicle;
+// }
+
+// Enums
+// nicht mit node ausführen sondern erst npm i tsx und dann npx tsx dateiname.ts
+
+function turn(direction: string): void{
+    if (direction === "left") {
+        console.log("Turning left");
+    }
+}
+
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right
+}
+let move: Direction = Direction.Up;
+
+enum EventStatus {
+    Draft = "draft",
+    Published = "published",
+    Cancelled = "cancelled",
+}
+
+function logStatus(status: EventStatus): void {
+    console.log(`Current status: ${status}`);
+}
+logStatus(EventStatus.Draft);
